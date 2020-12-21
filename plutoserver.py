527b184e1cffa454ff65560ebf04173d1bf2e4af
Fons van der Plas <fonsvdplas@gmail.com>
2020-12-22 00:08:08 +0100

    asdf

--- plutoserver.py
+++ plutoserver.py
@@ -2,7 +2,7 @@ from os.path import expanduser, join
 
 def setup_plutoserver():
   return {
-    "command": ["julia", "--optimize=0", "--project=pluto", "-e", "import Pluto; Pluto.run(host=\"0.0.0.0\", port={port}, require_secret_for_access=false, require_secret_for_open_links=false, open_browser=false)"],
+    "command": ["julia", "--optimize=0", "--project=pluto", "-e", "import Pluto; Pluto.run(host=\"0.0.0.0\", port={port}, require_secret_for_access=false, require_secret_for_open_links=false, launch_browser=false)"],
     "timeout": 60,
     "launcher_entry": {
         "title": "Pluto.jl",
